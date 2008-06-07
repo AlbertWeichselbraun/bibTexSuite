bc158a47dbdd3b04d3920131af5f832c09a74a99
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2008-06-07 17:59:30 +0000

    added support for a default format in the config file. improved handling of the output format.

--- bibSearch.py
+++ bibSearch.py
@@ -19,16 +19,18 @@ import os.path
 from sys import path
 from operator import attrgetter
 from optparse import OptionParser
-from bibtex import BibTex
 from glob import glob
 from os import stat
-from cache import cacheRetrieve
 
 LIB_DIR       = os.path.join(os.path.dirname(__file__), "lib")
 
-USER_PREF_DIR = os.path.expanduser("~/.bibSearch")
+USER_PREF_DIR = os.path.expanduser("~/.bibTexSuite")
 CONFIG_FILE   = os.path.join( USER_PREF_DIR, "searchconfig.py" )
 USER_CACHE    = os.path.join( USER_PREF_DIR, "cache" )
+
+OUTPUT_FORMAT = { 'citation' : 'getCitation',
+                  'wikipedia': 'getWikipediaCitation',
+                  'bibtex'   : 'getBibTexCitation' }
 
 def _create_config():
     """ creates the cache/configuration files """
@@ -36,7 +38,7 @@ def _create_config():
     if not os.path.exists(USER_CACHE)    : os.mkdir(USER_CACHE)
     if not os.path.exists(CONFIG_FILE):
         f=open( CONFIG_FILE, "w")
-        f.write("# bibSearch example config file\n\nBIB_SEARCH_PATH = ['%s', ]\n" % (os.path.expanduser("~")) )
+        f.write("# bibSearch example config file\n\n# default output format (citation, bibtex, wikipedia)\nDEFAULT_OUTPUT_FORMAT = 'citation'\n\nDEFAULT_BIB_SEARCH_PATH = ['%s', ]\n" % (os.path.expanduser("~")) )
         f.close()
 
 def _read_config():
@@ -53,7 +55,9 @@ def parse_options():
     parser = OptionParser()
     parser.add_option("-b", "--bibtex", dest="bibtex", action="store_true",
                       help="output search results as bibtex snippets.")
-    parser.add_option("-w", "--wiki", dest="wiki", action="store_true",
+    parser.add_option("-c", "--citation", dest="citation", action="store_true",
+                      help="output search results as wikipedia citations.")
+    parser.add_option("-w", "--wikipedia", dest="wikipedia", action="store_true",
                       help="output search results as wikipedia citations.")
     parser.add_option("-p", "--path", dest="path", action="append", default=[],
                       help="add additional paths to the default search path.")
@@ -61,15 +65,13 @@ def parse_options():
 
     (options, args) = parser.parse_args()
 
-    # choose the output format 
-    if options.bibtex:
-        output_format = 'getBibTexCitation'
-    elif options.wiki:
-        output_format = 'getWikipediaCitation'
-    else:
-        output_format = 'getCitation'
+    # output format 
+    try:
+        output = [ opt for opt in OUTPUT_FORMAT.iterkeys() if attrgetter(opt)(options) ][0]
+    except IndexError:
+        output = DEFAULT_OUTPUT_FORMAT or 'citation'
 
-    return {'output_format': output_format, 'search_terms': args, 'search_path': BIB_SEARCH_PATH + options.path }
+    return {'output_format': OUTPUT_FORMAT[output], 'search_terms': args, 'search_path': DEFAULT_BIB_SEARCH_PATH + options.path }
 
 
 _get_bib = lambda fn:  [ b for b in BibTex(fn) ]
@@ -91,7 +93,9 @@ def get_matching_bibtex_entries( search_terms, search_path ):
 # ===============================================================================
 
 _read_config()
-from searchconfig import BIB_SEARCH_PATH
+from searchconfig import DEFAULT_BIB_SEARCH_PATH, DEFAULT_OUTPUT_FORMAT
+from bibtex import BibTex
+from cache import cacheRetrieve
 
 opt = parse_options()
 entries = get_matching_bibtex_entries( opt['search_terms'], opt['search_path'] )
